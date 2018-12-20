e40e311ba63e8ee5a738f8989201927a95b0f03f
Ivar Farup <ivar.farup@ntnu.no>
2018-12-20 15:15:46 +0100

    01_python godt på vei

---
+++ 01_python/modul.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+def dobbel(x):
+    """
+    Beregner og returnerer den dobbelte verdien.
+    
+    Det er ikke mye å si om dette, men om man skulle ha lyst,
+    kan man skrive en lengre utlegning om temaet dobling her.
+    
+    Paramters
+    ---------
+    x : float / int
+        Tallet som skal dobles.
+    
+    Returns
+    -------
+    float / int
+        Den doblede verdien.
+    """
+    return 2 * x
+
+
+class test_modul(unittest.TestCase):
+
+    def test_dobbel(self):
+        self.assertTrue(dobbel(3) == 6)
+        self.assertTrue(dobbel(7.4) == 14.8)
