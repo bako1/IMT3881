43343f91492f1a034b9d521d6d212cdbd68ab9b6
Abdi_Bako <abdi.bako@gmail.com>
2020-01-10 08:10:41 +0100

    Arbeid krav 1

--- 01_python/modul.py
+++ 01_python/modul.py
@@ -1,28 +1,28 @@
 import unittest
+import numpy as np
 
-
-def dobbel(x):
+def f(x):
+#def dobbel(x):
     """
-    Beregner og returnerer den dobbelte verdien.
+    Beregner og returnerer summen av cos og sin.
 
-    Det er ikke mye å si om dette, men om man skulle ha lyst,
-    kan man skrive en lengre utlegning om temaet dobling her.
+    
 
     Paramters
     ---------
     x : float / int
-        Tallet som skal dobles.
+        
 
     Returns
     -------
     float / int
-        Den doblede verdien.
+        Den summerte verdien.
     """
-    return 2 * x
+    return np.sin(x)+np.cos(x)
 
 
 class test_modul(unittest.TestCase):
 
-    def test_dobbel(self):
-        self.assertEqual(dobbel(3), 6)
-        self.assertAlmostEqual(dobbel(7.4), 14.8)
+    def test_f(self):
+        self.assertEqual(f(0), 1)
+        self.assertAlmostEqual(f(1), 1.38177329068)
